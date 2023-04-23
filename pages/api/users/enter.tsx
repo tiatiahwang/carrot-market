8d7ef30981308f7a068368ce80965c82fd843d86
tiatiahwang <tiahwang44@gmail.com>
2023-04-23 16:03:30 -0700

    add: enter page server logic

--- pages/api/users/enter.tsx
+++ pages/api/users/enter.tsx
@@ -1,9 +1,22 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import withHandler from '@/libs/server/withHandler';
+import client from '@/libs/server/client';
 
 async function handler(req: NextApiRequest, res: NextApiResponse) {
-  console.log(req.body);
-  res.status(200).end();
+  const { email, phone } = req.body;
+  const payload = phone ? { phone: +phone } : { email };
+  const user = await client.user.upsert({
+    where: {
+      ...payload,
+    },
+    create: {
+      name: 'anonymous',
+      ...payload,
+    },
+    update: {},
+  });
+  console.log(user);
+  return res.status(200).end();
 }
 
 export default withHandler('POST', handler);
