72081b48ddfc8936df8e8fe1c9a1f5f54c6cf07a
tiatiahwang <tiahwang44@gmail.com>
2023-04-28 23:44:01 -0700

    add: SWR

--- libs/client/useUser.ts
+++ libs/client/useUser.ts
@@ -1,18 +1,12 @@
 import { useRouter } from 'next/router';
 import { useEffect, useState } from 'react';
+import useSWR from 'swr';
+
+const fetcher = (url: string) => fetch(url).then((response) => response.json());
 
 export default function useUser() {
+  const { data, error } = useSWR('/api/users/me', fetcher);
   const router = useRouter();
-  const [user, setUser] = useState();
-  useEffect(() => {
-    fetch('/api/users/me')
-      .then((response) => response.json())
-      .then((data) => {
-        if (!data.ok) {
-          return router.replace('/enter');
-        }
-        setUser(data.profile);
-      });
-  }, [router]);
-  return user;
+
+  return data;
 }
